372445f17203f523018677e693880c2658b9e23a
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2023-10-22 00:10:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,20 @@ const btn=document.querySelector(".btn-pick");
 const edit=document.querySelector(".edit-text");
 const footer=document.querySelector('footer');
 btn.addEventListener("click",clickie);
-const div=document.createElement("div");
-const input=document.createElement('input');
-const input_text=document.createElement('input');
-const Child_div=document.createElement("div");
-const button1=document.createElement('button');
-const button2=document.createElement('button');
-const input2=document.querySelector(".text-list");
 
 function clickie(event){
     event.preventDefault();
     var val=main_input.value;
+    const div=document.createElement("div");
     div.classList.add("info");
 
-
+    const input=document.createElement('input');
     input.setAttribute('type','checkbox');
-    input.classList.add("bubble");
+    input.classList.add('bubble');
+
     div.appendChild(input);
 
+    const input_text=document.createElement('input');
     input_text.setAttribute('type','text');
     input_text.value=val;
     input_text.classList.add("text-list");
@@ -28,15 +24,17 @@ function clickie(event){
 
     div.appendChild(input_text);
 
-
+    const Child_div=document.createElement("div");
     Child_div.classList.add("change");
 
-    
+    const button1=document.createElement('button');
     button1.innerText="Edit";
-    button1.classList.add('rem');
 
+    const button2=document.createElement('button');
     button2.innerText="Delete";
 
+    const input2=document.querySelector(".text-list");
+    
     Child_div.appendChild(button1);
     Child_div.appendChild(button2);
     div.appendChild(Child_div);
@@ -44,24 +42,5 @@ function clickie(event){
 
     main_input.value="";
 }
-// const butt=document.querySelector(".rem");
-// button1.addEventListener('click', (e) => {
-    
-//     const ini = div.querySelector('input');
-//     ini.setAttribute("readonly",false);
-//     // ini.removeAttribute('readonly');
-//     // ini.focus();
-//     // ini.addEventListener('blur', (e) => {
-//     //     ini.setAttribute('readonly', true);
-
-
-//     // alert("hey")
-// });
-// button1.addEventListener('click', (e) => {
-//     input_text.setAttribute('readonly', true);
-// });
-
-
-
 
 
